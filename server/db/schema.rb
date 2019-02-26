e65439c4a78d6bc5dcb57d0f7020e57676d18c0f
Ruby <ngoc@tinypulse.com>
2019-02-26 22:32:21 +0700

    Adding creating user

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_071403) do
+ActiveRecord::Schema.define(version: 2019_02_26_035145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2019_02_25_071403) do
     t.text "description"
     t.boolean "private", default: true
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
