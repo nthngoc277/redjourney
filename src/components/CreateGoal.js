d8dd833dc6ff87051ec8d1484a33f74ac2af9ab1
Ruby <ngoc@tinypulse.com>
2019-03-04 21:47:31 +0700

    Add task for creating goal

--- src/components/CreateGoal.js
+++ src/components/CreateGoal.js
@@ -19,13 +19,39 @@ const CREATE_GOAL_MUTATION = gql`
   }
 `
 class CreateGoal extends Component {
-  state = {
-    title: '',
-    description: ''
+  constructor() {
+    super()
+    this.state = {
+      title: '',
+      description: '',
+      tasks: [{ description: '' }]
+    }
   }
+  
+
+  handleTaskDescriptionChange = idx => event => {
+    const newTasks = this.state.tasks.map((task, task_idx) => {
+      if (idx !== task_idx) return task;
+      return { ...task, description: event.target.value };
+    });
+
+    this.setState({ tasks: newTasks });
+  };
+
+  handleRemoveTask = idx => () => {
+    this.setState({
+      tasks: this.state.tasks.filter((_task, task_idx) => idx !== task_idx)
+    });
+  };
+
+  handleAddTask = () => {
+    this.setState({
+      tasks: this.state.tasks.concat([{ description: '' }])
+    });
+  };
 
   render() {
-    const { title, description } = this.state
+    const { title, description, tasks } = this.state;
     return (
       <Fragment>
         <h2>Create goal</h2>
@@ -49,12 +75,46 @@ class CreateGoal extends Component {
             onChange={e => this.setState({description: e.target.value})}
           />
         </div>
+
+        <div class="form-group">
+          <label for="description">What will you do to complete this goal?</label>
+          {tasks.map((task, idx) => (
+            <div class="input-group task">
+              <input 
+                type="text" 
+                class="form-control"
+                placeholder={`Task #${idx + 1} description`}
+                onChange={this.handleTaskDescriptionChange(idx)}
+                value={task.description}
+              />
+              <div class="input-group-append">
+                <button 
+                  type="button"
+                  class="btn btn-outline-secondary"
+                  onClick={this.handleRemoveTask(idx)}
+                >
+                  -
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div class="form-group">
+          <button 
+            type="button" 
+            class="btn btn-btn btn-outline-secondary"
+            onClick={this.handleAddTask}
+          >
+            + Add more tasks
+          </button>
+        </div>
         <Mutation
           mutation={CREATE_GOAL_MUTATION} 
           variables={{ title, description }}
           onCompleted={() => this.props.history.push('/my-goals')}
         >
-          {postMutation => <button class="btn btn-primary" onClick={postMutation}>Create goal</button>}
+          {postMutation => <button class="btn btn-primary btn-block" onClick={postMutation}>Create goal</button>}
         </Mutation>
       </Fragment>
     )
